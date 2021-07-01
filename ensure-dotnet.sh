1a4e97377557f2574c7a1201a1d170020eb8e0fa
climr <stinsonbj@ornl.gov>
2021-07-01 16:37:09 -0400

    Update ensure-dotnet.sh

--- ensure-dotnet.sh
+++ ensure-dotnet.sh
@@ -2,11 +2,11 @@
 # usage:
 #   ensure-dotnet.sh
 #
-# This script ensures that dotnet is installed on the target environment
+# This script ensures that dotnet 5.0 is installed on the target environment
 
 DRY_RUN=false
 LOCAL=/usr/local
-DOTNET_DOWNLOAD=https://download.visualstudio.microsoft.com/download/pr/df1fe0a4-487f-4803-b541-4abb2f08ef2d/c905bb3ccc7f584963a45e32d7361d74/dotnet-sdk-3.1.409-linux-arm64.tar.gz
+DOTNET_5_DOWNLOAD=https://download.visualstudio.microsoft.com/download/pr/d0a22fa3-b916-49ce-8284-97131b424cb3/cb884163ad34b83f1ae1dbd33e09d77a/aspnetcore-runtime-5.0.7-linux-arm64.tar.gz
 DOTNET_RUNTIME=${LOCAL}/bin/dotnet/shared
 DOTNET_INSTALL=${LOCAL}/bin/dotnet
 H31=https://github.com/horiz31
@@ -21,7 +21,7 @@ if ! [ -d "${DOTNET_RUNTIME}" ] ; then
 	wget $DOTNET_DOWNLOAD -P /tmp
 	$SUDO mkdir -p ${DOTNET_INSTALL} && $SUDO chmod a+w $(dirname ${DOTNET_INSTALL})
 	echo "Uncompressing dotnet..."
-    $SUDO tar zxf /tmp/dotnet-sdk-3.1.409-linux-arm64.tar.gz -C ${DOTNET_INSTALL}
+    $SUDO tar zxf /tmp/aspnetcore-runtime-5.0.7-linux-arm64.tar.gz -C ${DOTNET_INSTALL}
 
 	# see if lines already added
 	x=$(cat ~/.bashrc | grep 'DOTNET_ROOT' | head -1)
